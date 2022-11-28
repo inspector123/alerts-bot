bce2ee14bcfa5dc715f1b94a1031145d71f008bd
inspector123 <7421886101a@gmail.com>
2022-11-27 19:56:25 -0800

    settimeout message

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const Telegraf = require('telegraf');
+const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('5802732074:AAFOFJTQf97hZyXZvrkLbLcsKGStBQOlw4Y');
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+    })
+    setTimeout(()=>bot.telegram.sendMessage(ctx.chat.id, 'check this out!! a thing happened!!!!'), 10000)
+})
+
+bot.launch();
