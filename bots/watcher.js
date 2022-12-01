2bd24f34b91435b96c715a2417b7e84b59be671f
inspector123 <7421886101a@gmail.com>
2022-12-01 11:57:31 -0800

    1inch, univ3 work, kyb needs eth fix

--- bots/watcher.js
+++ bots/watcher.js
@@ -115,78 +115,7 @@ export class Watcher {
 
     
 
-    async decodeUniV3Router2(txHash) {
-        try {
-            let tx = await this.web3Http.eth.getTransactionReceipt(txHash);
-            if (tx != null) {
-                
-                let { from, hash, to } = tx;
-                    if (tx.to.toLowerCase() == UniswapV3Router2.toLowerCase()) {
-                        console.log(txHash, tx.from, 'nonUniTX')
-                        return;
-                    }
-                        
-                    
-                if (tx.logs) {
-                    const swaps = []
-                    const swapPairUserLogs = tx.logs.map(log=>{
-                        log.address = log.address.toLowerCase();
-                        log.topics = log.topics.map(t=>t.replace('0x000000000000000000000000', '0x'));
-                        return log
-                    })
-                    .filter(log=>{
-                        // log.topics.includes(from) ||  && 
-                        return (
-                            ((log.topics.includes(from) && !log.topics.includes(tx.to)) || (log.address.toLowerCase() == WETHAddress && log.topics.includes(tx.to))) &&
-                            log.topics.length == 3 &&
-                        //exclude fee
-                            !log.topics.includes(log.address)
-                        )
-                    });
 
-                                        
-                    const swapSend = await Promise.all(swapPairUserLogs.filter(log=>{
-                        return log.topics[1] == tx.from || (log.address == WETHAddress && log.topics[1] == tx.to)
-                    }).map(async log=> {
-                        const contract = new this.web3Http.eth.Contract(tokenABI, log.address)
-                        const symbol = await contract.methods.symbol().call();
-                        return {
-                            contract,
-                            symbol,
-                            amount: new BigNumber(this.web3Http.utils.hexToNumberString(log.data)) / 10**(await contract.methods.decimals().call())
-                                }
-                    }))
-                    const swapReceive = await Promise.all(swapPairUserLogs.filter(log=>{
-                        return log.topics[2] == tx.from || (log.address == WETHAddress && log.topics[2] == tx.to)
-                    }).map(async log=> {
-                        const contract = new this.web3Http.eth.Contract(tokenABI, log.address)
-                        const symbol = await contract.methods.symbol().call();
-                        return {
-                            contract,
-                            symbol,
-                            amount: new BigNumber(this.web3Http.utils.hexToNumberString(log.data)) / 10**(await contract.methods.decimals().call())
-                            }
-                    }))
-                    const swapDetails = swapSend && swapReceive ? {sent: swapSend[0], received: swapReceive[0]}: []
-                    console.log(tx.from, swapDetails)
-                    let tokenPairContract, tokenContractAddress;
-                    if (swapDetails.sent.length && ["USDC","USDT","WETH"].includes(swapDetails.sent.symbol)) {
-                        tokenPairContract = await swapDetails.received.contract.methods.uniswapV2Pair().call();
-                        tokenContractAddress = swapDetails.received.contract.address;
-                    }
-                    if (swapDetails.received.length && ["USDC","USDT","WETH"].includes(swapDetails.received.symbol)) {
-                        console.log('asdklfj')
-                        tokenPairContract = await swapDetails.sent.contract.methods.uniswapV2Pair().call();
-                        tokenContractAddress = swapDetails.sent.contract.address;
-                    }
-                    
-                    sendTelegramSwapMessage(bot,ctx,tx,swapDetails, tokenPairContract, tokenContractAddress)
-                }
-            }
-        } catch(e) {
-            console.log(e)
-        }
-    }
 
     async decodeLogs(txHash) {
         try {
@@ -194,11 +123,6 @@ export class Watcher {
             if (tx != null) {
                 
                 let { from, hash, to } = tx;
-                    if (tx.to.toLowerCase() == UniswapV3Router2.toLowerCase()) {
-                        console.log(txHash, tx.from, 'nonUniTX')
-                        return;
-                    }
-                        
                     
                 if (tx.logs) {
                     const swaps = []
@@ -243,17 +167,18 @@ export class Watcher {
                     const swapDetails = swapSend && swapReceive ? {sent: swapSend[0], received: swapReceive[0]}: []
                     console.log(tx.from, swapDetails)
                     let tokenPairContract, tokenContractAddress;
-                    if (swapDetails.sent && swapDetails.received) {
-                        if (swapDetails.sent && ["USDC","USDT","WETH"].includes(swapDetails.sent.symbol)) {
-                            tokenPairContract = await swapDetails.received.contract.methods.uniswapV2Pair().call();
-                            tokenContractAddress = swapDetails.received.contract.address;
-                        }
-                        if (swapDetails.received && ["USDC","USDT","WETH"].includes(swapDetails.received.symbol)) {
-                            console.log('asdklfj')
-                            tokenPairContract = await swapDetails.sent.contract.methods.uniswapV2Pair().call();
-                            tokenContractAddress = swapDetails.sent.contract.address;
-                        }
-                    }
+                    // if (swapDetails.sent && swapDetails.received) {
+                    //     if (swapDetails.sent && ["USDC","USDT","WETH"].includes(swapDetails.sent.symbol)) {
+                    //         tokenPairContract = await swapDetails.received.contract.methods.uniswapV2Pair().call();
+                    //         tokenContractAddress = swapDetails.received.contract.address;
+                    //     }
+                    //     if (swapDetails.received && ["USDC","USDT","WETH"].includes(swapDetails.received.symbol)) {
+                    //         console.log('asdklfj')
+                    //         swapDetails.rec
+                    //         tokenPairContract = await swapDetails.sent.contract.methods.uniswapV2Pair().call();
+                    //         tokenContractAddress = swapDetails.sent.contract.address;
+                    //     }
+                    // }
                     
                     this.sendTelegramSwapMessage(tx,swapDetails, tokenPairContract, tokenContractAddress)
                 }
@@ -292,35 +217,47 @@ export class Watcher {
                 if (block) {
                     let { transactions } = block;
                     _transactions = transactions;
+                    //send usdt
+                    //'0x6e7291f3270074f030b7ed6c831d78097c73e0c8785f474be0ea4600ec6cd028'
+                    //receive eth?
+
+                    //                    //weth to pepebet
+                    //0xb76d3c3e4aeb2bb399be4a4510c28a60ed9b453b009d404ab07e05fb4afd5dda
+
+                    //pepebet to weth
+                    //"0x15561e64745c81d4c5927044373027117219eab3e5ce78261144027a32c1e8d4",
+
+                    //usdc to pepebet
+                    //"0x8544eac09dc26ab8eddf524d2cf5b6ed8c64d5c5fd9c9fea411bbf528d516d38",
+
+                    //usdt to pepebet
+                    //"0x3a0fed98c8e96c6c41cb13a51cc8b5faa5dddefd0d7e3fa913d66f5bcbe39c9b",
+
+                    //pepebet to usdt
+                    //"0x57e36692a244acb165b0993dcbc085f536931c26834829dcc14319c4fb5b68df",
+
+                    //1inch agix to weth
+                    //"0x6e9c18fcc16b5282ba040631edfffd0a5c688467a83175c8eb4910be4d841481",
+                    //kyberswap eth to chz
+                    //"0xb91b9492fa90f73bfebf48a89bc8467091f97953dec954d587946d642259c8c2",
+                    transactions = [
+//                         '0x6e7291f3270074f030b7ed6c831d78097c73e0c8785f474be0ea4600ec6cd028', 
+//                     "0xb76d3c3e4aeb2bb399be4a4510c28a60ed9b453b009d404ab07e05fb4afd5dda",
+//                     "0x15561e64745c81d4c5927044373027117219eab3e5ce78261144027a32c1e8d4",
+// "0x8544eac09dc26ab8eddf524d2cf5b6ed8c64d5c5fd9c9fea411bbf528d516d38",
+// "0x3a0fed98c8e96c6c41cb13a51cc8b5faa5dddefd0d7e3fa913d66f5bcbe39c9b",
+// "0x57e36692a244acb165b0993dcbc085f536931c26834829dcc14319c4fb5b68df",
+"0x6e9c18fcc16b5282ba040631edfffd0a5c688467a83175c8eb4910be4d841481",
+"0xb91b9492fa90f73bfebf48a89bc8467091f97953dec954d587946d642259c8c2"
+                    
+                ]
                     transactions.forEach(async (txHash, index) => {
                         setTimeout(async ()=>{
 
-                            this.decodeLogs('0x6e7291f3270074f030b7ed6c831d78097c73e0c8785f474be0ea4600ec6cd028')
-                            //see tx.to from getTransaction
-
-                            //if tx.to is from UniV3 getTransactionReceipt
-
-                            //if tx.to is univ2 just use tx.input
-
-                            // switch(tx.to) {
-                            //     case UniswapV2:
-                            //         this.decodeUniV2(tx);
-                            //         break;
-                            //     case UniswapV3Router2:
-                            //         this.decodeUniV3Router2(tx);
-                            //         break;
-                            //     case KyberSwap: 
-                            //         this.decodeKyberSwap(tx);
-                            //         break;
-                            //     case OneInchv5Router:
-                            //         this.decode1Inchv5Router(tx);
-                            //         break;
-                            //     default:
-                            //         break;
-                                //find out what happens when someone sends a 
-                                //token
+                            this.decodeLogs(txHash)
                             
-                        }, index*2500)
+                            
+                        }, index*50)
                         
                     })
                 }
@@ -331,24 +268,18 @@ export class Watcher {
             }
         })
     }
-    
+    // SHOULD HAVE SENT TO, RECEIVED FROM
     sendTelegramSwapMessage = (tx, swapDetails, tokenPairContract, tokenContractAddress) => {
         
             this.volumeBot.telegram.sendMessage(this.chatId, 
-    `New Transaction from \`${tx.from}\`! 
+    `New Transaction from \`\`\`${tx.from}\`\`\`! 
 TX HASH: https://etherscan.io/tx/${tx.transactionHash}
-
 Details: 
-${swapDetails.sent ? `Sent: ${swapDetails.sent?.amount} ${swapDetails.sent?.symbol}` : ``}
-
+${swapDetails.sent ? `Sent: ${swapDetails.sent?.amount} ${swapDetails.sent?.symbol}` : ``} /
 ${swapDetails.received ? `Received: ${swapDetails.received?.amount} ${swapDetails.received?.symbol}` : ``}
 ${tokenPairContract ? `Dextools: https://dextools.io/app/ether/pair-explorer/${tokenPairContract}` : ``}
-
 ${tokenPairContract ? `Contract Address: https://etherscan.io/token/${tokenContractAddress}` : ``}
 Wallet Link: https://etherscan.io/address/${tx.from}
-        
-            
-            
             `)
         // } else if (tx.to == OneInchv5Router) {
         //     this.alertBot.telegram.sendMessage(this.chatId, `New 1Inchv5 Transaction from ${tx.from}!
