f68af20a5e5cd09ab2c2b6a0d72e3475fc2a9705
inspector123 <7421886101a@gmail.com>
2022-12-06 11:09:37 -0800

    add more stuff

--- start.js
+++ start.js
@@ -1,15 +1,28 @@
 import { Watcher } from './bots/watcher.js'
 import wallets from "./bots/wallets.js"
+import express from "express"
+import cors from "cors"
+import router from './routes'
+import AppError from "./utils/appError";
+import errorHandler from "./utils/errorHandler";
 const AlertBotKey = '5802732074:AAFOFJTQf97hZyXZvrkLbLcsKGStBQOlw4Y';
 const VolumeBotKey = "5974906041:AAFGovND1cdDX3dPZ3TBgXnweO6Zv89oZAE";
+
+//
+
+app.use(api, router);
+
+app.all("*", (req, res, next) => {
+ next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
+});
+app.use(errorHandler);
+const PORT = 3000;
+app.listen(PORT, () => {
+ console.log(`server running on port ${PORT}`);
+});
+
 
 const CHAT_ID_CHANNEL = -1001855095247;
 const CHAT_ID_DISCUSSION = -1001882676825;
 
 const watcher = new Watcher(CHAT_ID_DISCUSSION, wallets, AlertBotKey, VolumeBotKey, false);
-
-//send bot commands through watcher
-
-//??
-
-//
